6d7ee41d7a0eb98157997b4e1f4f8eaee19dbbde
Felipe Eduardo <pinheiro.eduardo@escolar.ifrn.edu.br>
2026-05-21 15:37:07 -0300

    Update personagem.py

--- personagem.py
+++ personagem.py
@@ -1,2 +1,28 @@
-class personagem:
-  def __init__(self, movimentacao, dano, vida)
+class Jogador:
+
+    def __init__(self, pos_x, pos_y):
+
+        self.vidas = 5
+        self.dano_soco = 10
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+
+    def receber_dano(self):
+
+        self.vidas -= 1
+
+        print(f"O jogador recebeu dano!")
+        print(f"Vidas restantes: {self.vidas}")
+      
+        if self.vidas <= 0:
+            print("O jogador morreu!")
+
+    def soco(self, inimigo):
+
+        inimigo.vida -= self.dano_soco
+
+        print(f"O jogador deu um soco em {inimigo.nome}!")
+        print(f"Vida do inimigo: {inimigo.vida}")
+
+        if inimigo.vida <= 0:
+            print(f"{inimigo.nome} foi derrotado!")
