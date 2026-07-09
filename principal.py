be918ace883db8d23f290aa995c6e8399aae84dc
Jose-sigma <neto.ferreira1@escolar.ifrn.edu.br>
2026-07-09 14:23:27 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -3,19 +3,7 @@ from inimigos import InimigoInvestidor, InimigoAtirador
 from item import PowerUpDano, FragmentoMemoria
 import tela
 
-player = Jogador(pos_x=100, pos_y=100)
-touro = InimigoInvestidor(nome="Touro", dano=1, vida=50, raio_visao=150, pos_x=50, pos_y=100, velocidade_corrida=30, tempo_atordoado=2)
-mago = InimigoAtirador(nome="Mago", dano=1, vida=40, raio_visao=200, pos_x=180, pos_y=100, alcance_ataque=150)
-power = PowerUpDano()
 fragmento = FragmentoMemoria(fase=1, descricao="Uma memória antiga foi recuperada.")
-
-print("===== JOGO INICIADO (CONSOLE) =====")
-power.ativar(player)
-player.soco(touro)
-touro.investir(player)
-player.receber_dano()
-power.desativar(player)
-mago.atacar_distancia(player)
 
 print("\n===== INICIANDO INTERFACE GRÁFICA =====")
 if __name__ == "__main__":
