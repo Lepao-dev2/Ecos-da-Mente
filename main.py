006debd664a78396ac9406c57ee7d86c5021298b
Felipe Eduardo <pinheiro.eduardo@escolar.ifrn.edu.br>
2026-07-19 17:01:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-from personagem import Jogador
-from inimigos import InimigoInvestidor, InimigoAtirador
-from item import PowerUpDano, FragmentoMemoria
-import tela
+from game import Game
 
-fragmento = FragmentoMemoria(fase=1, descricao="Uma memória antiga foi recuperada.")
-
-print("\n===== INICIANDO INTERFACE GRÁFICA =====")
-if __name__ == "__main__":
-    tela.menu()
+if _name_ == "_main_":
+    game = Game()
+    game.run()
